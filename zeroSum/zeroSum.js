eb60ac9dbefcf3b78748e9307edd2504537b174a
bilel <hb.bilel@gmail.com>
2019-12-31 09:16:58 +0100

    done!

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,17 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+function zeroSum(arr) {
+    let obj = {}
+    //let sum = Math.sum(...arr)
+    for (let i = 0; i < arr.length; i++){
+        if (obj[arr[i]]) {
+            return true
+        }
+        else {
+            obj[-arr[i]]= 1
+        }
+    }
+    return false
+}
