8abcc137f11762312429a7b65fa012f92abd8c7b
bilel <hb.bilel@gmail.com>
2020-01-03 09:12:13 +0100

    done !

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -1,7 +1,8 @@
 /*
 Longest Run
 
-Write a function that, given a string, finds the longest run of identical characters and returns an array containing the start and end indices of that run. 
+Write a function that, given a string, finds the longest run of identical characters and
+ returns an array containing the start and end indices of that run. 
 If there are two runs of equal length, return the first one. Return [0,0] for no runs.
 
 longestRun("abbbcc") // [ 1, 3 ]
@@ -12,4 +13,21 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+var longestRun = function (string) {
+    let result = [0,0];
+    let longest = 1;
+    let start = 0;
+    let x = 1;
+    let current;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] !== current) {
+          x > longest ? (longest = x , result = [start, i - 1]): current = string[i], start = i , x = 1
+      } else {
+        x++
+          if (i === string.length - 1) {
+           x > longest ? result = [start, i]:null
+        }
+      }
+    }
+    return string.length > 0 ? result : null;
+  };
